766b7015cee4d79266acdb74ea4b5a02ab87c77d
Cameron Harris <charris@planview.com>
2021-05-24 21:26:21 -0500

    moved user_data to a seperate file and updated readme

--- main.tf
+++ main.tf
@@ -1,42 +1,40 @@
+variable "server_name" {
+  type = string
+  description = "Name of ec2 instance"
+}
+
+variable "instance_size" {
+    type = string
+    description = "ec2 instance size"
+    default = "t4g.large"
+}
+
+variable "drive_size" {
+  type = number
+  description = "ebs volume size"
+  default = 100
+}
+
+variable "region" {
+  type = string
+  description = "AWS region to provision in"
+  default = "us-west-2"
+}
+
+variable "key_name" {
+  type = string
+  description = "ssh key for ec2 instance"
+}
+
+
 provider "aws" {
-    region = "us-west-2"
+    region = var.region
 }
 
 locals {
-    user_data = <<EOF
-#!/bin/bash
-apt update
-apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-    gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | \
- tee /etc/apt/sources.list.d/kubernetes.list
-
-apt-get update
-
-apt-get install -y docker-ce docker-ce-cli containerd.io kubectl
-
-curl -Lo /tmp/kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-arm64
-chmod +x /tmp/kind
-mv /tmp/kind /usr/bin/kind
-
-
-
-EOF
+    user_data = file("userdata.sh")
 }
+
 
 data "aws_vpc" "default" {
     default = true
@@ -80,11 +78,11 @@ module "ec2" {
     instance_count      = 1
     name                = "kind-k8s-cluster-instance"
     ami                 = "ami-09d9c897fc36713bf" #ubuntu server 20.04 LTS
-    instance_type       = "t4g.large"
+    instance_type       = var.instance_size
     subnet_id           = tolist(data.aws_subnet_ids.all.ids)[0]
     vpc_security_group_ids      = [module.security_group.security_group_id]
     associate_public_ip_address = true
-    key_name            = "default_aws_key"
+    key_name            = var.key_name
 
     user_data_base64 = base64encode(local.user_data)
 
@@ -92,7 +90,7 @@ module "ec2" {
     root_block_device = [
         {
         volume_type = "gp2"
-        volume_size = 100
+        volume_size = var.drive_size
         tags = {
             Name = "my-root-block"
         }
